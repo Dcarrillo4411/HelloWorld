8a94e7e9c4749394a0ec0f90e0c869f99a0cd9dd
DanielCarrillo <daniel.carrillo4411@outlook.com>
2024-11-20 23:58:02 -0600

    init

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    // Agregamos metodo principal main
+    // psvm es el atajo para crear un main
+    public static void main(String[] args) {
+        //sout es el atajo para crear un println
+        System.out.println("mensaje de prueba");
+    }
+}
